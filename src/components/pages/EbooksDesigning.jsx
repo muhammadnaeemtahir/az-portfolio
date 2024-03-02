28329d640b71a8562c4e1c8588379538cf117886
muhammadnaeemtahir <m.naeemtahir@hotmail.com>
2024-03-02 22:42:21 +0500

    fix: ebook thumbnails

--- src/components/pages/EbooksDesigning.jsx
+++ src/components/pages/EbooksDesigning.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../Hero';
-import Ebooks from '../../data/Ebooks.json';
+
+import Ebooks from '../../data/Ebooks.json'
 
 const EbooksDesigning = () => {
-
     const heroContent = {
         title: 'eBooks Designing',
         subtitle: `I have designed eBooks for various clients in different niches. I have designed eBooks for
-        clients in the following niches:`,
+         clients in the following niches:`,
     };
 
     return (
@@ -16,8 +17,8 @@ const EbooksDesigning = () => {
             <section className='my-5'>
                 <div className="container portfolio">
                     <div className="row">
-                        {Ebooks.map((ebook, index) => (
-                            <div className="col-md-4 col-sm-6 col-12 mb-3" key={index}>
+                        {Ebooks.map((ebook) => (
+                            <div className="col-md-4 col-sm-6 col-12 mb-3" key={ebook.id}>
                                 <Link to={`/ebook-details/${ebook.id}`}>
                                     <div className="portfolio-item mx-auto card ebook-card">
                                         <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
@@ -29,11 +30,8 @@ const EbooksDesigning = () => {
                                             src={ebook.thumbnail}
                                             alt={ebook.title}
                                             className="img-fluid rounded ebook-img"
-                                            style={{
-                                                objectFit: 'cover',
-                                                height: '280px',
-                                            }}
-                                            loading='lazy'
+                                            style={{ objectFit: 'cover', height: '280px' }}
+                                            loading="lazy"
                                         />
                                     </div>
                                 </Link>

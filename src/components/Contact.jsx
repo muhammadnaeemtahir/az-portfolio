74b61072c056ccb0d1bc6ceb9bfc77b6eaa35a40
Muhammad Naeem Tahir <naeem.tahir@funavry.com>
2024-09-08 19:47:58 +0500

    changes in contact heading

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
     return (
         <section className="page-section" id="contact">
             <div className="container">
-                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Us</h2>
+                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
                 <div className="divider-custom">
                     <div className="divider-custom-line"></div>
                     <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
